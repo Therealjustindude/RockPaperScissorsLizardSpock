0d3f94aa0063ba04d4ccd8db7a628280c747c83d
Justin Davies <therealjustindude@gmail.com>
2024-12-18 12:49:07 -0500

    initial commit project set up

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+  import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <header>
+    <img alt="Game logo" class="logo" src="@/assets/logo-bonus.svg" width="125" height="125" />
+  </header>
+  <main>
+    <HelloWorld msg="You did it!" />
+  </main>
+  <footer>
+    <HelloWorld msg="You did it!" />
+  </footer>
+</template>
+
+<style scoped>
+</style>
